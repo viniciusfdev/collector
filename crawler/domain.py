960c1c82dd6a97472f5d0ecbd9343b0fb9fcee68
Vinicius F <vinilima07@gmail.com>
2020-09-16 22:08:28 -0300

    atividade 2

--- crawler/domain.py
+++ crawler/domain.py
@@ -1,28 +1,19 @@
 from datetime import datetime
 
 class Domain():
-	def __init__(self, nam_domain, int_time_limit_between_requests):
-		self.time_last_access = datetime(1970,1,1)
+	def __init__(self, nam_domain):
 		self.nam_domain = nam_domain
-		self.int_time_limit_seconds  = int_time_limit_between_requests
-	@property
-	def time_since_last_access(self):
-		pass
-
-	def accessed_now(self):
-		pass
-
-	def is_accessible(self):
-		return False
 
 	def __hash__(self):
-		return None
+		return hash(self.nam_domain)
 
 	def __eq__(self, domain):
-		return None
+		if isinstance(domain, Domain):
+			return domain.nam_domain == self.nam_domain
+		return domain == self.nam_domain
 
 	def __str__(self):
 		return self.nam_domain
 
 	def __repr__(self):
-		return str(self)
+		return f"{self.nam_domain}"
